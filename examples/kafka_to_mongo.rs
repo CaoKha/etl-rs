cc2868fe6da19faa05256e65352c08eb4e9583b7
CaoKha <nguyencaokha131995@gmail.com>
2024-07-04 17:19:14 +0200

    add csv-to-postgres

--- examples/kafka_to_mongo.rs
+++ examples/kafka_to_mongo.rs
@@ -1,9 +1,13 @@
+use std::env;
+
 use artemis_rs::config::{Config, KafkaMessage};
 use artemis_rs::kafka::create_kafka_base_consumer;
-use log::{error, info};
+use log::{error, info, warn};
+use mongodb::{bson, Client};
 use rdkafka::consumer::{BaseConsumer, Consumer};
 use rdkafka::topic_partition_list::Offset;
 use rdkafka::{Message, TopicPartitionList};
+use serde_json::Value;
 
 async fn read_messages_from_offset_range(
     consumer: &BaseConsumer,
@@ -15,6 +19,7 @@ async fn read_messages_from_offset_range(
     let mut messages: Vec<KafkaMessage> = Vec::new();
     let mut topic_partition = TopicPartitionList::new();
     let _ = topic_partition.add_partition_offset(topic, partition, Offset::Offset(start_offset));
+
     consumer
         .assign(&topic_partition)
         .expect("Failed to get partition offset");
@@ -23,7 +28,10 @@ async fn read_messages_from_offset_range(
         let msg = message.expect("Failed to get message");
         let payload = match msg.payload_view::<str>() {
             Some(Ok(s)) => s.to_string(),
-            _ => continue,
+            _ => {
+                warn!("Payload not found");
+                continue;
+            }
         };
 
         let kafka_message = KafkaMessage {
@@ -46,8 +54,30 @@ async fn read_messages_from_offset_range(
     messages
 }
 
+async fn save_kafka_messages_to_mongo(
+    client: &mongodb::Client,
+    db_name: &str,
+    collection_name: &str,
+    messages: &[KafkaMessage],
+) -> mongodb::error::Result<()> {
+    let db = client.database(db_name);
+    let coll = db.collection(collection_name);
+    let mut docs: Vec<bson::Document> = Vec::new();
+
+    for message in messages {
+        let value_json = serde_json::from_str::<Value>(&message.value)
+            .expect("Cannot serialize from str to json");
+        let doc = bson::to_document(&value_json)?;
+        docs.push(doc);
+    }
+
+    coll.insert_many(docs).await?;
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
     env_logger::init();
 
     let config = match Config::load("config.json") {
@@ -61,9 +91,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     info!("Configuration loaded successfully");
 
     let consumer = create_kafka_base_consumer(&config.kafka.bootstrap_servers, "test-group");
-    let messages = read_messages_from_offset_range(&consumer, &config.kafka.topic, 0, 228, 284).await;
-
-    info!("Messages: {:?}", messages);
+    let messages =
+        read_messages_from_offset_range(&consumer, &config.kafka.topic, 0, 296, 341).await;
+    info!("Messages: {:#?}", messages);
+    let mongo_uri = format!(
+        "mongodb://{}:{}@{}:{}/",
+        env::var("MONGO_ROOT_USR")?,
+        env::var("MONGO_ROOT_PWD")?,
+        env::var("MONGO_HOST")?,
+        env::var("MONGO_PORT")?
+    );
+    let mongo_client = Client::with_uri_str(mongo_uri).await?;
+    save_kafka_messages_to_mongo(&mongo_client, "client_raw", "recette_brut", &messages).await?;
 
     Ok(())
 }
--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,53 @@
 use serde::{Deserialize, Serialize};
-use std::fs;
 use std::error::Error;
+use std::fs;
 
-#[derive(Debug, Deserialize)]
-pub struct KafkaConfig {
-    pub bootstrap_servers: String,
-    pub topic: String,
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct JddSchema {
+    #[serde(rename = "RAISON_SOCIALE")]
+    pub raison_sociale: Option<String>,
+
+    #[serde(rename = "SIRET")]
+    pub siret: Option<String>,
+
+    #[serde(rename = "SIREN")]
+    pub siren: Option<String>,
+
+    #[serde(rename = "APE")]
+    pub ape: Option<String>,
+
+    #[serde(rename = "CODE_NAF")]
+    pub code_naf: Option<String>,
+
+    #[serde(rename = "LIBELE_NAF")]
+    pub libele_naf: Option<String>,
+
+    #[serde(rename = "CIVILITE")]
+    pub civilite: Option<String>,
+
+    #[serde(rename = "NOM")]
+    pub nom: Option<String>,
+
+    #[serde(rename = "PRENOM")]
+    pub prenom: Option<String>,
+
+    #[serde(rename = "TELEPHONE")]
+    pub telephone: Option<String>,
+
+    #[serde(rename = "email")]
+    pub email: Option<String>,
+
+    #[serde(rename = "address")]
+    pub address: Option<String>,
+
+    #[serde(rename = "CODE POSTALE")]
+    pub code_postale: Option<String>,
+
+    #[serde(rename = "REGION")]
+    pub region: Option<String>,
+
+    #[serde(rename = "PAYS")]
+    pub pays: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -15,6 +57,12 @@ pub struct KafkaMessage {
     pub topic: String,
     pub partition: i32,
     pub offset: i64,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct KafkaConfig {
+    pub bootstrap_servers: String,
+    pub topic: String,
 }
 
 #[derive(Debug, Deserialize)]
