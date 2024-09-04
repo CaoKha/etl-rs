8271931ad2e6a3042ea613f1ed82a12638eae1b1
CaoKha <nguyencaokha131995@gmail.com>
2024-09-04 16:09:48 +0200

    finish hdd_deduplication

--- examples/transform_hdd_deduplication.rs
+++ examples/transform_hdd_deduplication.rs
@@ -15,27 +15,37 @@ use polars::lazy::dsl::{col, concat_list, lit};
 use polars::prelude::*;
 use sea_query::{ColumnRef, PostgresQueryBuilder, Query};
 use sqlx::PgPool;
-use std::collections::HashSet;
 use std::env;
 
 fn transform_deduplication(lf: LazyFrame) -> PolarsResult<LazyFrame> {
-    let mut original_lf = lf.clone();
+    let lf_original = lf.clone();
     // Self join dataframe to find duplications
-    let mut lf = lf
+    let mut lf_deduplicating = lf
         .clone()
         .cross_join(lf, Some(String::from("_right")))
         .filter(
-            col(Hdd::Id.as_str())
-                .lt(col(format!("{}_{}", Hdd::Id.as_str(), "right").as_str())) // use
-                // less than to remove combinations with the same Id element but with different
-                // order
+            col(Hdd::Siret.as_str())
+                .is_null()
+                .and(
+                    col(Hdd::Id.as_str())
+                        .lt(col(format!("{}_{}", Hdd::Id.as_str(), "right").as_str())), // use
+                                                                                        // less than to remove combinations with the same Id element but with different
+                                                                                        // order
+                )
                 .and(
                     col(Hdd::Nom.as_str())
                         .eq(col(format!("{}_{}", Hdd::Nom.as_str(), "right").as_str())),
                 )
-                .and(col(Hdd::Prenom.as_str()).eq(col(
-                    format!("{}_{}", Hdd::Prenom.as_str(), "right").as_str(),
-                )))
+                .and(
+                    (col(Hdd::Prenom.as_str()).eq(col(format!(
+                        "{}_{}",
+                        Hdd::Prenom.as_str(),
+                        "right"
+                    )
+                    .as_str())))
+                    .or(col(Hdd::Prenom.as_str()).is_null())
+                    .or(col(format!("{}_{}", Hdd::Prenom.as_str(), "right").as_str()).is_null()),
+                )
                 .and(
                     col(Hdd::Pce.as_str())
                         .eq(col(format!("{}_{}", Hdd::Pce.as_str(), "right").as_str()))
@@ -115,42 +125,73 @@ fn transform_deduplication(lf: LazyFrame) -> PolarsResult<LazyFrame> {
             .alias(Hdd::RaisonSociale.as_str()),
         ]);
 
+    println!(
+        "lf_deduplicating stage 1: {:#?}",
+        lf_deduplicating
+            .clone()
+            .select([
+                col(Hdd::Nom.as_str()),
+                col(Hdd::Prenom.as_str()),
+                col(Hdd::Ids.as_str())
+            ])
+            .collect()
+    );
+
     // Grouping by Hdd::Id to find deduplicates
-    lf = lf
+    lf_deduplicating = lf_deduplicating
         .group_by([Hdd::Id.as_str(), Hdd::Nom.as_str(), Hdd::Prenom.as_str()])
         .agg([
             col(Hdd::Pce.as_str()).flatten().alias(Hdd::Pce.as_str()),
             col(Hdd::Email.as_str())
                 .flatten()
+                .unique()
                 .alias(Hdd::Email.as_str()),
             col(Hdd::Telephone.as_str())
                 .flatten()
+                .unique()
                 .alias(Hdd::Telephone.as_str()),
-            col(Hdd::Ids.as_str()).flatten().alias(Hdd::Ids.as_str()),
             col(Hdd::Siret.as_str())
                 .flatten()
+                .unique()
                 .alias(Hdd::Siret.as_str()),
             col(Hdd::SiretSuccesseur.as_str())
                 .flatten()
+                .unique()
                 .alias(Hdd::SiretSuccesseur.as_str()),
             col(Hdd::RaisonSociale.as_str())
                 .flatten()
+                .unique()
                 .alias(Hdd::RaisonSociale.as_str()),
+            col(Hdd::Ids.as_str())
+                .flatten()
+                .unique()
+                .alias(Hdd::Ids.as_str()),
             col(Hdd::IdSource.as_str())
                 .flatten()
+                .unique()
                 .alias(Hdd::IdSource.as_str()),
         ]);
 
-    // Remove any rows that have the same set JDD::UIDS or is a subset of another set of JDD::UIDS
-    let lf_subsets = lf
+    println!(
+        "lf_deduplicating stage 2: {:#?}",
+        lf_deduplicating.clone().collect()
+    );
+
+    // Get any rows that have the same set JDD::UIDS or is a subset of another set of JDD::UIDS
+    let lf_deduplicating_subsets = lf_deduplicating
         .clone()
-        .cross_join(lf.clone(), Some(String::from("_right")))
+        .cross_join(lf_deduplicating.clone(), Some(String::from("_right")))
         .filter(
-            col(Hdd::Ids.as_str())
-                .list()
-                .set_difference(col(format!("{}_{}", Hdd::Ids.as_str(), "right").as_str()))
-                .len()
-                .eq(0),
+            col(Hdd::Id.as_str())
+                .neq(col(format!("{}_{}", Hdd::Id.as_str(), "right").as_str()))
+                .and(
+                    col(Hdd::Ids.as_str())
+                        .list()
+                        .set_difference(col(format!("{}_{}", Hdd::Ids.as_str(), "right").as_str()))
+                        .list()
+                        .len()
+                        .eq(0),
+                ),
         )
         .select([
             col(Hdd::Id.as_str()),
@@ -166,28 +207,38 @@ fn transform_deduplication(lf: LazyFrame) -> PolarsResult<LazyFrame> {
             col(Hdd::IdSource.as_str()),
         ]);
 
-    lf = lf
-        .join(
-            lf_subsets,
-            [col(Hdd::Id.as_str())],
-            [col(Hdd::Id.as_str())],
-            JoinArgs::new(JoinType::Anti),
-        )
-        .select([
-            col(Hdd::Id.as_str()),
-            col(Hdd::Nom.as_str()),
-            col(Hdd::Prenom.as_str()),
-            col(Hdd::Pce.as_str()),
-            col(Hdd::Email.as_str()),
-            col(Hdd::Telephone.as_str()),
-            col(Hdd::Ids.as_str()),
-            col(Hdd::Siret.as_str()),
-            col(Hdd::SiretSuccesseur.as_str()),
-            col(Hdd::RaisonSociale.as_str()),
-            col(Hdd::IdSource.as_str()),
-        ]);
+    println!(
+        "lf_deduplicating_subsets: {:#?}",
+        lf_deduplicating_subsets.clone().collect()
+    );
 
-    let ids_to_remove = lf
+    // Remove those rows that have the same set JDD::UIDS or is a subset of another set of JDD::UIDS from lf_self_joined
+    lf_deduplicating = lf_deduplicating.join(
+        lf_deduplicating_subsets,
+        [col(Hdd::Id.as_str())],
+        [col(Hdd::Id.as_str())],
+        JoinArgs::new(JoinType::Anti),
+    );
+
+    let lf_rows_to_add = lf_deduplicating.clone().select([
+        col(Hdd::Id.as_str()),
+        col(Hdd::Nom.as_str()),
+        col(Hdd::Prenom.as_str()),
+        col(Hdd::Pce.as_str()).list().join(lit("/"), false),
+        col(Hdd::Email.as_str()).list().join(lit("/"), false),
+        col(Hdd::Telephone.as_str()).list().join(lit("/"), false),
+        col(Hdd::Siret.as_str()).list().join(lit("/"), false),
+        col(Hdd::SiretSuccesseur.as_str())
+            .list()
+            .join(lit("/"), false),
+        col(Hdd::RaisonSociale.as_str())
+            .list()
+            .join(lit("/"), false),
+        col(Hdd::IdSource.as_str()).list().join(lit("/"), false),
+        col(Hdd::Ids.as_str()).list().join(lit("/"), false),
+    ]);
+
+    let vec_ids_to_remove = lf_deduplicating
         .clone()
         .select([col(Hdd::Ids.as_str()).flatten().unique()])
         .collect()?
@@ -195,17 +246,54 @@ fn transform_deduplication(lf: LazyFrame) -> PolarsResult<LazyFrame> {
         .str()?
         .into_iter()
         .filter_map(|opt_id| opt_id.map(|id| id.to_string()))
-        .collect::<Vec<_>>();
+        .collect::<Vec<String>>();
 
-    println!("Id to remove: {:#?}", ids_to_remove);
+    println!("Id to remove: {:#?}", vec_ids_to_remove);
 
-    let ids_to_remove_series = Series::new("ids_to_remove", ids_to_remove);
+    let series_ids_to_remove = Series::new("ids_to_remove", vec_ids_to_remove);
 
-    original_lf = original_lf.filter(col(Hdd::Id.as_str()).is_in(lit(ids_to_remove_series)));
+    let lf_row_to_remove = lf_original
+        .clone()
+        .filter(col(Hdd::Id.as_str()).is_in(lit(series_ids_to_remove)));
 
-    println!("LF after removed : {:#?}", original_lf.clone().collect());
+    let lf_original_with_removed_rows = lf_original
+        .with_column(lit(NULL).cast(DataType::String).alias(Hdd::Ids.as_str()))
+        .join(
+            lf_row_to_remove,
+            [col(Hdd::Id.as_str())],
+            [col(Hdd::Id.as_str())],
+            JoinArgs::new(JoinType::Anti),
+        );
 
-    Ok(original_lf)
+    println!(
+        "lf_original_with_removed_rows: {:#?}",
+        lf_original_with_removed_rows.clone().collect()
+    );
+
+    println!("lf_rows_to_add: {:#?}", lf_rows_to_add.clone().collect());
+
+    let exprs_columns_to_select = [
+        col(Hdd::Id.as_str()),
+        col(Hdd::Nom.as_str()),
+        col(Hdd::Prenom.as_str()),
+        col(Hdd::Pce.as_str()),
+        col(Hdd::Email.as_str()),
+        col(Hdd::Telephone.as_str()),
+        col(Hdd::Ids.as_str()),
+        col(Hdd::Siret.as_str()),
+        col(Hdd::SiretSuccesseur.as_str()),
+        col(Hdd::RaisonSociale.as_str()),
+        col(Hdd::IdSource.as_str()),
+    ];
+    let lf_final = concat(
+        [
+            lf_original_with_removed_rows.select(exprs_columns_to_select.clone()),
+            lf_rows_to_add.select(exprs_columns_to_select),
+        ],
+        UnionArgs::default(),
+    )?;
+
+    Ok(lf_final)
 }
 
 #[tokio::main]
@@ -238,14 +326,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         col(Hdd::Id.as_str()),
     ]);
 
-    df = transform_deduplication(lf)?
-        .select(&[
-            col(Hdd::Nom.as_str()),
-            col(Hdd::Prenom.as_str()),
-            col(Hdd::Id.as_str()),
-            col(Hdd::Ids.as_str()).list().join(lit("/"), true),
-        ])
-        .collect()?;
+    df = transform_deduplication(lf)?.collect()?;
     println!("Deduplication: {:#?}", df);
 
     let mut csv_file =
