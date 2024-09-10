160df221ea422e4855266a1800b0d3ad00640490
CaoKha <nguyencaokha131995@gmail.com>
2024-09-10 17:57:05 +0200

    pwd wip

--- crates/libs/lib-auth/src/pwd/scheme/scheme_01.rs
+++ crates/libs/lib-auth/src/pwd/scheme/scheme_01.rs
@@ -42,3 +42,26 @@ fn hash(key: &[u8], to_hash: &ContentToHash) -> Result<String> {
 	let result = b64u_encode(result_bytes);
 	Ok(result)
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::{config::auth_config, pwd::ContentToHash};
+	use uuid::Uuid;
+	pub type Error = Box<dyn std::error::Error>;
+	pub type Result<T> = std::result::Result<T, Error>;
+
+	#[test]
+	fn test_scheme_01_hash_into_b64u_ok() -> Result<()> {
+		let fx_salt = Uuid::parse_str("f05e8961-d6ad-4086-9e78-a6de065e5453")?;
+		let fx_key = &auth_config().PWD_KEY;
+		let fx_to_hash = ContentToHash {
+			content: "hello world".to_string(),
+			salt: fx_salt,
+		};
+		let fx_res = "qO9A90161DoewhNXFwVcnAaljRIVnajvd5zsVDrySCwxpoLwVCACzaz-8Ev2ZpI8RackUTLBVqFI6H5oMe-OIg";
+		let res = hash(fx_key, &fx_to_hash)?;
+		assert_eq!(res, fx_res);
+		Ok(())
+	}
+}
